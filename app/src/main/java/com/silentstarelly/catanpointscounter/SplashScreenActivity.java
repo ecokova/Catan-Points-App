bc708d4e5f4602de085ef451f223a8ed3e6108f4
= <elena.cokova@tufts.edu>
2016-12-31 22:50:16 -0500

    Performance improvements for splash page

--- app/src/main/java/com/silentstarelly/catanpointscounter/SplashScreenActivity.java
+++ app/src/main/java/com/silentstarelly/catanpointscounter/SplashScreenActivity.java
@@ -1,12 +1,17 @@
 package com.silentstarelly.catanpointscounter;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class SplashScreenActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -20,8 +25,30 @@ public class SplashScreenActivity extends AppCompatActivity implements View.OnCl
 
         Button enterButton = (Button) findViewById(R.id.enter_button);
         enterButton.setOnClickListener(this);
+
     }
 
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight);
+            final int widthRatio = Math.round((float) width / (float) reqWidth);
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+
+        return inSampleSize;
+    }
     @Override
     public void onClick(View v) {
         Intent intent = new Intent(this, GameCreateActivity.class);
